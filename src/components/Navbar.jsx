8e2acc2e2fef8bcc1c1223d5f7d979b169ed5fb7
Ashishvelhal <ashishvelhal147@gmail.com>
2025-11-30 23:28:49 +0530

    ,

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ export default function Navbar({ setAuth }) {
         Dashboard
       </h1>
       <button onClick={handleLogout} className="px-4 py-2 rounded-lg bg-red-600 hover:bg-red-700 text-white font-semibold shadow-lg">
-ww        Logout
+        Logout
       </button>
     </header>
   );
